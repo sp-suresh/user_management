839bf6c9cbe7529547709817a39ecb0fd90bab72
sureshzebpay <suresh.p@zebpay.com>
2019-10-15 02:56:06 +0530

    Added env

---
+++ env.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    NODE_ENV: 'development',
+    PORT: 4400,
+    MONGODB_URI: 'mongodb+srv://assignment_user:virtusa1234@brahmos-t9icy.mongodb.net/adminPanel?retryWrites=true&w=majority',
+    MONGODB_USER: 'assignment_user',
+    MONGODB_PASSWORD: 'virtusa1234',
+    JWT_SECRET: 'strong_sceret'
+  },
+  production: {
+    
+  }
+}
