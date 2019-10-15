2b3568290789a2cb2800ece4ee57ad6d32aabc0a
Suresh Prajapati <sureshpraja1234@gmail.com>
2019-10-15 16:48:14 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     NODE_ENV: 'development',
-    PORT: 80,
+    PORT: 4400,
     MONGODB_URI: 'mongodb+srv://assignment_user:virtusa1234@brahmos-t9icy.mongodb.net/adminPanel?retryWrites=true&w=majority',
     MONGODB_USER: 'assignment_user',
     MONGODB_PASSWORD: 'virtusa1234',
